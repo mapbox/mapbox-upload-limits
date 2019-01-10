d87d90847868f19ae8c4a56374a07092122efbe4
Arni Sumarlidason <arni.sumarlidason@mapbox.com>
2019-01-10 16:57:14 -0500

    support os env variables for overriding defaults
    support os env variables for overriding defaults
    
    refs #10
    
    * add ability replace default limits w/ os env variables
    * default limits are retained @ ._defaults
    * add tests for limit replacement
    * load json file w/ `fs` in lieu of `require`
    
    support os env variables for overriding defaults
    
    refs #10
    
    * move fastlog from devDependencies to dependencies
    * specify encoding

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-module.exports = require('./limits.json');
+const fs     = require('fs');
+const limits = JSON.parse(fs.readFileSync('limits.json', 'utf-8'));
+const log    = require('fastlog')('configuration', 'debug');
+
+module.exports = (() => {
+    limits._defaults = JSON.parse(JSON.stringify(limits));
+    let envKey, envValue;
+    for(let limitType in limits) {
+        for(let limitKey in limits[limitType]) {
+            envKey   = `LIMITS_${limitType.toUpperCase()}_${limitKey.toUpperCase()}`;
+            envValue = process.env[envKey];
+            if(typeof envValue !== 'undefined') {
+                if(envValue.length === 0) {
+                    log.warn(`Environment variable specified '${envKey}', but has no value. The default (${limits[limitType][limitKey]}) will be used.`);
+                    continue;
+                }
+                if(isNaN(envValue)) {
+                    log.warn(`Environment variable specified '${envKey}', but value is invalid: '${envValue}'. The default (${limits[limitType][limitKey]}) will be used.`);
+                    continue;
+                }
+
+                limits[limitType][limitKey] = +envValue;
+            }
+        }
+    }
+    return limits;
+})();
--- test.js
+++ test.js
@@ -1,3 +1,48 @@
-var assert = require('assert');
-assert(require('./'));
-console.log('✌');
+const test = require('tape');
+const log  = require('fastlog')('test');
+
+test('module can be imported', (t) => {
+    let limits = require('./');
+    t.ok(limits);
+    t.ok(Object.keys(limits).length > 0, 'limits loaded');
+    t.end();
+});
+
+test('override limits with environment variables: valid', (t) => {
+    clearFromCache(t);
+    process.env.LIMITS_MBTILES_MAX_FILESIZE = '1024';
+    const limits = require('./');
+    t.equals(limits.mbtiles.max_filesize, +process.env.LIMITS_MBTILES_MAX_FILESIZE);
+    t.end();
+});
+
+test('override limits with environment variables: empty', (t) => {
+    clearFromCache(t);
+    process.env.LIMITS_MBTILES_MAX_FILESIZE = '';
+    const limits = require('./');
+    t.equals(limits.mbtiles.max_filesize, limits._defaults.mbtiles.max_filesize);
+    t.end();
+});
+
+test('override limits with environment variables: malformed', (t) => {
+    clearFromCache(t);
+    process.env.LIMITS_MBTILES_MAX_FILESIZE = 'abcdef';
+    const limits = require('./');
+    t.equals(limits.mbtiles.max_filesize, limits._defaults.mbtiles.max_filesize);
+    t.end();
+});
+
+const clearFromCache = (t) => {
+    let k;
+    for(let loadedModule in require.cache) {
+        if(loadedModule.endsWith('/mapbox-upload-limits/index.js')) {
+            k = loadedModule;
+            break;
+        }
+    }
+    t.ok(k, 'require cache cleared');
+    if(k) {
+        delete require.cache[k];
+    }
+}
+
